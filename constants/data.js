ae1cdde84e7da2ccd75f57f9f505098a4c56435d
crmonlinegems <pureworldnew@gmail.com>
2023-04-20 05:40:30 -0500

    initial commit

---
+++ constants/data.js
@@ -0,0 +1,93 @@
+const userData = {
+  githubUsername: "pureworldnew",
+  name: "Jonathan Samayoa",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.jpg",
+  email: "jonathandreamdev@gmail.com",
+  phone: "+1 8065761063",
+  address: "Amarillo, Texas, US.",
+  projects: [
+    {
+      title: "With-meetwithanyone.com",
+      link: "https://meetwithanyone.com/",
+      imgUrl: "/meetwithanyone.jpg",
+    },
+    {
+      title: "sky republic inc",
+      link: "https://www.skyrepublic.com/",
+      imgUrl: "/sky.jpg",
+    },
+    {
+      title: "Mend",
+      link: "https://mend.com/",
+      imgUrl: "/mend.jpg",
+    },
+    {
+      title: "UCLA",
+      link: "https://www.ucla.edu/",
+      imgUrl: "/ucla.jpg",
+    },
+    {
+      title: "Bio Rad",
+      link: "https://www.bio-rad.com/",
+      imgUrl: "/biorad.jpg",
+    },
+    {
+      title: "Carters",
+      link: "https://www.carters.com/home?id=carters",
+      imgUrl: "/carters.jpg",
+    },
+    {
+      title: "Kaiser",
+      link: "https://healthy.kaiserpermanente.org/southern-california/front-door",
+      imgUrl: "/kaiser.jpg",
+    },
+    {
+      title: "USI",
+      link: "https://www.usi.com/",
+      imgUrl: "/usi.jpg",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 12 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Node.js, Express, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Python, PHP, Java programming language etc to name a few. Although I barely know the workflow of (CI/CD pipeline, Docker, Kubernetes!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "With-meetwithanyone.com",
+    currentProjectUrl: "https://meetwithanyone.com/",
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "With-meetwithanyone.com",
+      year: "2028-2023",
+      companyLink: "https://meetwithanyone.com/",
+      desc: "Face-to-face, online experiences with professionals, experts, influencer, and more.",
+    },
+    {
+      title: "Software Developer",
+      company: "Sky Republic Inc",
+      year: "2014-2018",
+      companyLink: "https://www.skyrepublic.com/",
+      desc: "Designed to get multi-enterprise applications up and running quickly, Sky Republic makes your supply chain more productive, resilient, and nimble.",
+    },
+    {
+      title: "Graduation",
+      company: "University of Texas at Dallas",
+      year: "2014",
+      companyLink: "https://www.utdallas.edu/",
+      desc: "Major in Computer Science and Software Engineering.",
+    }
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1t2_xv_kVQRt8xtaY1BYLaWWESHbBGnJR/view",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/jonathan-samayoa/",
+    github: "https://github.com/pureworldnew",
+  },
+};
+
+export default userData;
